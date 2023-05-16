8f49b17f728e9929ac7caa1a6d58ab09a935a2a5
Metaljisawa <48813296+Metaljisawa@users.noreply.github.com>
2023-05-17 00:59:12 +0200

    Update Win10.psm1

--- Win10.psm1
+++ Win10.psm1
@@ -2298,7 +2298,12 @@ Function UnpinStartMenuTiles {
 			$data = (Get-ItemProperty -Path "$($_.PsPath)\Current" -Name "Data").Data -Join ","
 			$data = $data.Substring(0, $data.IndexOf(",0,202,30") + 9) + ",0,202,80,0,0"
 			Set-ItemProperty -Path "$($_.PsPath)\Current" -Name "Data" -Type Binary -Value $data.Split(",")
-		
+		}
+	} ElseIf ([System.Environment]::OSVersion.Version.Build -ge 17134) {
+		$key = Get-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\CloudStore\Store\Cache\DefaultAccount\*start.tilegrid`$windows.data.curatedtilecollection.tilecollection\Current"
+		$data = $key.Data[0..25] + ([byte[]](202,50,0,226,44,1,1,0,0))
+		Set-ItemProperty -Path $key.PSPath -Name "Data" -Type Binary -Value $data
+		Stop-Process -Name "ShellExperienceHost" -Force -ErrorAction SilentlyContinue
 	}
 }
 
