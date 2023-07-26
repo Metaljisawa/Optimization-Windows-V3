6040b0bd8881ef52567644381d090f99900a1305
Metaljisawa <48813296+Metaljisawa@users.noreply.github.com>
2023-07-26 19:04:35 +0200

    Update Win10.ps1

--- Win10.ps1
+++ Win10.ps1
@@ -4,9 +4,7 @@
 ##########
 
 Function RequireAdmin {
-	If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
-		Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" $PSCommandArgs" -Verb RunAs
-		Exit
+	
 	}
 }
 
